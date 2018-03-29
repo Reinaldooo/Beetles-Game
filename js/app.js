6fa82fc6cc7ea860a7985a1ad891bb2a4ce9933a
Reinaldo Trindade <rewifetri@gmail.com>
2018-03-29 20:30:19 -0300

    Gems will only show up if a player is defined

--- js/app.js
+++ js/app.js
@@ -74,7 +74,7 @@ class Gem {
     }
 
     render() {
-        this.x != undefined && ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        (char != undefined && this.x != undefined) && ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     };
 
     update() {
